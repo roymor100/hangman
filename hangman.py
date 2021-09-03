e606b1cabd1bc3f585657d0a9b6db7dca728902c
Roy Mor <roymor100@gmail.com>
2021-09-03 20:09:50 +0300

    finished version sent to course grade & certificate

--- hangman.py
+++ hangman.py
@@ -1,5 +1,14 @@
-from colorama import Style, Fore
+"""
+Hangman game:
+The word or phrase to guess is represented by the row of dashes, one for
+each letter.
+Type the letter of the alphabet that you want to guess.
+Correct guesses will appear in the word or phrase.
+For each incorrect guess, you will get closer to being hung.
+"""
+
 import logging
+from colorama import Style, Fore
 
 MAX_TRIES = 6
 
@@ -14,23 +23,20 @@ WELCOME_LOGO = r"""
                         __/ |
                         |___/"""
 
-
 GAME_OVER = r"""
-  __ _   __ _  _ __ ___    ___     ___  __   __  ___  _ __ 
+  __ _   __ _  _ __ ___    ___     ___  __   __  ___  _ __
  / _` | / _` || '_ ` _ \  / _ \   / _ \ \ \ / / / _ \| '__|
-| (_| || (_| || | | | | ||  __/  | (_) | \ V / |  __/| |   
- \__, | \__,_||_| |_| |_| \___|   \___/   \_/   \___||_|   
-  __/ |                                                    
+| (_| || (_| || | | | | ||  __/  | (_) | \ V / |  __/| |
+ \__, | \__,_||_| |_| |_| \___|   \___/   \_/   \___||_|
+  __/ |
  |___/ """
 
-
 WINNER = r"""
- __      __  ___   _  _   _  _   ___   ___ 
+ __      __  ___   _  _   _  _   ___   ___
  \ \    / / |_ _| | \| | | \| | | __| | _ \
   \ \/\/ /   | |  | .` | | .` | | _|  |   /
    \_/\_/   |___| |_|\_| |_|\_| |___| |_|_\
 """
-
 
 HANGMAN_PHOTOS = {
     0: r"""    x-------x""",
@@ -74,21 +80,22 @@ HANGMAN_PHOTOS = {
 
 
 def print_logo():
-    """Prints the game's logo and the max tries for the player
+    """Prints the game's logo and the max tries for the player.
+
     :return: None
     """
-    print(WELCOME_LOGO)
-    print(MAX_TRIES)
+    print(f"{Fore.BLUE} {WELCOME_LOGO} {Style.RESET_ALL}")
+    print(f"{Fore.BLUE} {MAX_TRIES} {Style.RESET_ALL}\n")
 
 
 def check_win(secret_word: str, old_letters_guessed: list) -> bool:
-    """
-    Checks if the player guessed all the letters contained in the secret word
+    """Checks if guessed all the letters contained in the secret word.
+
     :param secret_word: The secret word chosen according to the index number
     :type secret_word: str
-    :param old_letters_guessed: list of letters that were already guessed by the player
+    :param old_letters_guessed: list of letters that were already guessed
     :type old_letters_guessed: list
-    :return: True is the player guessed al letters in secret word or False if not
+    :return: True is the player guessed all letters in secret word or False if not
     :rtype: bool
     """
     chars = ''
@@ -103,8 +110,8 @@ def check_win(secret_word: str, old_letters_guessed: list) -> bool:
 
 
 def show_hidden_word(secret_word: str, old_letters_guessed: list) -> str:
-    """
-    Shows the hidden word. letters that are not guessed are replaced with "_"
+    """Shows the hidden word. letters that are not guessed are replaced with "_".
+
     :param secret_word: The secret word chosen according to the index number
     :type secret_word: str
     :param old_letters_guessed: list of letters that were already guessed by the player
@@ -122,13 +129,13 @@ def show_hidden_word(secret_word: str, old_letters_guessed: list) -> str:
 
 
 def check_valid_input(letter_guessed, old_letters_guessed):
-    """
-    checks that the input is a one English letter and also that this letter was not guessed before
+    """Checks input is one English letter and letter was not guessed before.
+
     :param letter_guessed:
     :type letter_guessed: str
     :param old_letters_guessed: list of letters that were already guessed by the player
     :type old_letters_guessed: list
-    :return: True or False
+    :return: True if the letter is valid or False if not
     :rtype: bool
     """
     if not letter_guessed.isalpha():
@@ -138,28 +145,28 @@ def check_valid_input(letter_guessed, old_letters_guessed):
 
 
 def try_update_letter_guessed(letter_guessed: str, old_letters_guessed: list) -> bool:
-    """
-    Checks if the letter is English alphabet and was not guessed already
+    """Checks if the letter is English alphabet and was not guessed already.
+
     used the check_valid_input function
     :param letter_guessed: the letter that is guessed by the player
     :type letter_guessed: str
     :param old_letters_guessed: list of letters that were already guessed by the player
     :type old_letters_guessed: list
-    :return: True or False
+    :return: True if the letter is valid and was not already guessed or False if not
     :rtype: bool
     """
     if check_valid_input(letter_guessed, old_letters_guessed):
         old_letters_guessed.append(letter_guessed)
         return True
-    else:
-        print("X")
-        print("->".join(sorted(old_letters_guessed)))
-        return False
+
+    print("X")
+    print("->".join(sorted(old_letters_guessed)))
+    return False
 
 
 def choose_word(file_path: str, index: int) -> str:
-    """
-    Chooses a word from file according to the index number
+    """Chooses a word from file according to the index number.
+
     :param file_path: the location of the text file containing the words
     :type file_path: str
     :param index: the relevant index number of the word
@@ -168,18 +175,23 @@ def choose_word(file_path: str, index: int) -> str:
     :rtype: str
     """
     with open(file_path, "r", encoding='UTF8') as input_file:
-        word_list = input_file.read().splitlines()
-    word = word_list[index]
+        lines_list = input_file.read().splitlines()
+
+    word_list = []
+    for item in lines_list:
+        word_list.extend(item.split())
+
+    word = word_list[index % len(word_list) - 1]
     return word
 
 
 def main():
+    """main function"""
     # logging.basicConfig(level=logging.DEBUG)
     old_letters_guessed = []
     num_of_tries = 0
     print_logo()
-    # file_path = input("Enter file path: ")
-    file_path = r"""C:\python\words.txt"""
+    file_path = input("Enter file path: ")
     index_word = int(input("Enter index: "))
     secret_word = choose_word(file_path, index_word)
     logging.debug(secret_word)
@@ -190,10 +202,11 @@ def main():
     while True:
         letter_guessed = input("Guess a letter: ")
         letter_valid = try_update_letter_guessed(letter_guessed, old_letters_guessed)
-        logging.debug(f"old_letters_guessed :{old_letters_guessed}\n")
+        logging.debug(f"old_letters_guessed: {old_letters_guessed}\n")  # pylint: disable=logging-fstring-interpolation
 
         # invalid input
         if not letter_valid:
+            # The relevant print was done already at try_update_letter_guessed
             continue
 
         # wrong guess
